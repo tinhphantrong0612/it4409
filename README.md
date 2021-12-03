85e6f2b853805451db487eb5752f6689e4fc4638
TinhPT <tinh.phantrong0612@gmail.com>
2021-12-03 20:42:16 +0700

    Serve static files

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Bài tập lớn Công nghệ Web và dịch vụ trực tuyến
 # Start server
 npm run start
+Dùng live server add-on để sửa front-end
--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const path = require('path')
 
 const app = express();
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './front/index.html')));
-
+app.use(express.static('front'))
 app.listen(3000);
 
 console.log("App listen at 3000");
