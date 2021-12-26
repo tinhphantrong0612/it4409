5a51010b47cc341f7cdfbf990f9992703f164228
TinhPT <tinh.phantrong0612@gmail.com>
2021-12-26 20:03:43 +0700

    Database

--- initDB.sql
+++ initDB.sql
@@ -1,117 +1,116 @@
--- -----------------------------------------------------
-CREATE DATABASE `storagemanagement`;
+﻿-- -----------------------------------------------------
+CREATE DATABASE `it4409`;
 
-USE `storagemanagement`;
-
--- -----------------------------------------------------
--- Table `storagemanagement`.`customer`
--- -----------------------------------------------------
-CREATE TABLE `storagemanagement`.`customer` (
-  `Id` INT(11) NOT NULL AUTO_INCREMENT,
-  `DisplayName` VARCHAR(50) NOT NULL,
-  `Address` VARCHAR(200) NOT NULL,
-  `Phone` VARCHAR(20) NOT NULL,
-  `Email` VARCHAR(50) NOT NULL,
-  `MoreInfo` VARCHAR(200),
-  `ContractDate` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (`Id`)
-);
+USE `it4409`;
 
 -- -----------------------------------------------------
--- Table `storagemanagement`.`export`
+-- Table `it4409`.`unit`
 -- -----------------------------------------------------
-CREATE TABLE `storagemanagement`.`export` (
-  `Id` VARCHAR(36) NOT NULL,
-  `ExportDate` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (`Id`)
-);
-
--- -----------------------------------------------------
--- Table `storagemanagement`.`unit`
--- -----------------------------------------------------
-CREATE TABLE `storagemanagement`.`unit` (
-  `Id` INT(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `it4409`.`unit` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
   `DisplayName` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`Id`)
 );
 
 -- -----------------------------------------------------
--- Table `storagemanagement`.`supplier`
+-- Table `it4409`.`object`
 -- -----------------------------------------------------
-CREATE TABLE `storagemanagement`.`supplier` (
-  `Id` INT(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `it4409`.`object` (
+  `Id` VARCHAR(36) NOT NULL,
+  `DisplayName` VARCHAR(200) NOT NULL,
+  `UnitId` INT NOT NULL,
+  PRIMARY KEY (`Id`),
+  FOREIGN KEY (`UnitId`) REFERENCES `it4409`.`unit` (`Id`)
+);
+
+-- -----------------------------------------------------
+-- Table `it4409`.`customer`
+-- -----------------------------------------------------
+CREATE TABLE `it4409`.`customer` (
+  `Id` VARCHAR(36) NOT NULL,
+  `DisplayName` VARCHAR(50) NOT NULL,
+  `Address` VARCHAR(200) NOT NULL,
+  `Phone` VARCHAR(20) NOT NULL,
+  `Email` VARCHAR(50) NOT NULL,
+  `MoreInfo` varchar(200) NULL DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+);
+
+-- -----------------------------------------------------
+-- Table `it4409`.`export`
+-- -----------------------------------------------------
+CREATE TABLE `it4409`.`export` (
+  `Id` VARCHAR(36) NOT NULL,
+  `ExportDate` DATE NOT NULL,
+  `CustomerId` varchar(36) NOT NULL,
+  PRIMARY KEY (`Id`),
+  FOREIGN KEY (`CustomerId`) REFERENCES `it4409`.`customer` (`Id`)
+);
+
+-- -----------------------------------------------------
+-- Table `it4409`.`exportinfo`
+-- -----------------------------------------------------
+CREATE TABLE `it4409`.`exportinfo` (
+  `Id` VARCHAR(36) NOT NULL,
+  `ObjectId` VARCHAR(36) NOT NULL,
+  `ExportId` VARCHAR(36) NOT NULL,
+  `Amount` INT NOT NULL,
+  `ExportPrice` int NOT NULL,
+  PRIMARY KEY (`Id`),
+  FOREIGN KEY (`ObjectId`) REFERENCES `it4409`.`object` (`Id`),
+  FOREIGN KEY (`ExportId`) REFERENCES `it4409`.`export` (`Id`)
+);
+
+-- -----------------------------------------------------
+-- Table `it4409`.`supplier`
+-- -----------------------------------------------------
+CREATE TABLE `it4409`.`supplier` (
+  `Id` varchar(36) NOT NULL,
   `DisplayName` VARCHAR(50) NOT NULL,
   `Address` VARCHAR(200) NOT NULL,
   `Phone` VARCHAR(20) NOT NULL,
   `Email` VARCHAR(30) NOT NULL,
   `MoreInfo` VARCHAR(200) NULL DEFAULT NULL,
-  `ContractDate` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   PRIMARY KEY (`Id`)
 );
 
 -- -----------------------------------------------------
--- Table `storagemanagement`.`object`
+-- Table `it4409`.`import`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `storagemanagement`.`object` (
+CREATE TABLE `it4409`.`import` (
   `Id` VARCHAR(36) NOT NULL,
-  `DisplayName` VARCHAR(200) NOT NULL,
-  `Unit_Id` INT(11) NOT NULL,
-  `Supplier_Id` INT(11) NOT NULL,
-  `QRCode` VARCHAR(10) NULL DEFAULT NULL,
-  `Barcode` VARCHAR(10) NULL DEFAULT NULL,
+  `ImportDate` DATE NOT NULL,
+  `SupplierId` varchar(36) NOT NULL,
   PRIMARY KEY (`Id`),
-  FOREIGN KEY (`Unit_Id`) REFERENCES `storagemanagement`.`unit` (`Id`),
-  FOREIGN KEY (`Supplier_Id`) REFERENCES `storagemanagement`.`supplier` (`Id`)
+  FOREIGN KEY (`SupplierId`) REFERENCES `it4409`.`supplier` (`Id`)
 );
 
 -- -----------------------------------------------------
--- Table `storagemanagement`.`exportinfo`
+-- Table `it4409`.`importinfo`
 -- -----------------------------------------------------
-CREATE TABLE `storagemanagement`.`exportinfo` (
+CREATE TABLE `it4409`.`importinfo` (
   `Id` VARCHAR(36) NOT NULL,
-  `Object_Id` VARCHAR(36) NOT NULL,
-  `Export_Id` VARCHAR(36) NOT NULL,
-  `Customer_Id` INT(11) NOT NULL,
-  `Amount` INT(11) NOT NULL,
-  `Status` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`Id`),
-  FOREIGN KEY (`Object_Id`) REFERENCES `storagemanagement`.`object` (`Id`),
-  FOREIGN KEY (`Export_Id`) REFERENCES `storagemanagement`.`export` (`Id`),
-  FOREIGN KEY (`Customer_Id`) REFERENCES `storagemanagement`.`customer` (`Id`)
-);
-
--- -----------------------------------------------------
--- Table `storagemanagement`.`import`
--- -----------------------------------------------------
-CREATE TABLE `storagemanagement`.`import` (
-  `Id` VARCHAR(36) NOT NULL,
-  `ImportDate` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  PRIMARY KEY (`Id`)
-);
-
--- -----------------------------------------------------
--- Table `storagemanagement`.`importinfo`
--- -----------------------------------------------------
-CREATE TABLE `storagemanagement`.`importinfo` (
-  `Id` VARCHAR(36) NOT NULL,
-  `Object_Id` VARCHAR(36) NOT NULL,
-  `Import_Id` VARCHAR(36) NOT NULL,
-  `Amount` INT(11) NOT NULL,
+  `ObjectId` VARCHAR(36) NOT NULL,
+  `ImportId` VARCHAR(36) NOT NULL,
+  `Amount` INT NOT NULL,
   `ImportPrice` FLOAT NOT NULL,
-  `ExportPrice` FLOAT NOT NULL,
-  `Status` VARCHAR(50) NOT NULL,
+  `Barcode` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`Id`),
-  FOREIGN KEY (`Object_Id`) REFERENCES `storagemanagement`.`object` (`Id`),
-  FOREIGN KEY (`Import_Id`) REFERENCES `storagemanagement`.`import` (`Id`)
+  FOREIGN KEY (`ObjectId`) REFERENCES `it4409`.`object` (`Id`),
+  FOREIGN KEY (`ImportId`) REFERENCES `it4409`.`import` (`Id`)
 );
 
 -- -----------------------------------------------------
--- Table `storagemanagement`.`user`
+-- Table `it4409`.`user`
 -- -----------------------------------------------------
-CREATE TABLE `storagemanagement`.`user` (
-  `Id` INT(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `it4409`.`user` (
+  `Id` INT NOT NULL AUTO_INCREMENT,
   `DisplayName` VARCHAR(50) NOT NULL,
   `Username` VARCHAR(50) NOT NULL,
   `Password` VARCHAR(200) NOT NULL,
   PRIMARY KEY (`Id`)
 );
+
+INSERT INTO `it4409`.`user` (`DisplayName`, `Username`, `Password`) VALUES ('Phan Trọng Tình', 'tinh', '1');
+INSERT INTO `it4409`.`user` (`DisplayName`, `Username`, `Password`) VALUES ('Bùi Hoàng Long', 'long', '1');
+INSERT INTO `it4409`.`user` (`DisplayName`, `Username`, `Password`) VALUES ('Lưu Hồng Đức', 'duc', '1');
